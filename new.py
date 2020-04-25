66ce20b5014eb1e222b02eca021840c1347abfb5
Joshua <Jplittle2001@gmail.com>
2020-04-25 04:54:17 -0400

    added template cloner

---
+++ new.py
@@ -0,0 +1,14 @@
+import shutil
+
+def file_find_and_replace(file, old, new):
+    with open(file, 'r') as f:
+        data = f.read()
+    data = data.replace(old, new)
+    with open(file, 'w') as f:
+        f.write(data)
+
+name = input('> ').replace(' ', '_')
+title = name.replace('_', ' ').title()
+shutil.copytree('./src/template/', f'./src/{name}/')
+file_find_and_replace(f'./src/{name}/index.html', 'TEMPLATE_NAME', title)
+file_find_and_replace(f'./src/{name}/main.js', 'TEMPLATE_NAME', title)
